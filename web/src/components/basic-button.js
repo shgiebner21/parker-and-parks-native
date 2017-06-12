643505f5d32a6abf072449e67990328d8f1ad2cf
Stephen Giebner <shgiebner21@gmail.com>
2017-06-12 11:13:25 -0400

    initial styling of landing page

--- web/src/components/basic-button.js
+++ web/src/components/basic-button.js
@@ -4,8 +4,8 @@ const BasicButton = (props) => {
   return(
     <button
   onClick={props.onClick}
-  className="f6 no-underline white-90 bg-green bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4 br3">
-  <span className="pl1">{props.children}</span>
+  cls="f6 no-underline white-90 bg-green bg-animate hover-bg-black hover-white inline-flex items-center pa3 ba border-box mr4 br3">
+  <span cls="pl1">{props.children}</span>
 </button>
   )
 }
--- web/src/pages/landing.js
+++ web/src/pages/landing.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import { View, ListView, Text, StyleSheet, Image } from 'react-native'
+import { View, ListView, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import NativeTachyons, { wrap } from 'react-native-style-tachyons'
+import { Octicons } from '@expo/vector-icons'
 import Welcome from '../components/welcome'
 import BasicButton from '../components/basic-button'
 
@@ -9,11 +10,21 @@ NativeTachyons.build({ rem: 16}, StyleSheet)
 
 class LandingPage extends Component {
   render () {
+
+    const txt = `Hi!  I'm Parker Bear.`
+    const txt2 = `There are TWO great parks to play in so let's get started!`
+
     return (
-      <View style={styles.container} >
+      <View >
         <Image source={require('../hampton-park-bandstand.jpg')}
              style={styles.backgroundImage} />
-        <Text>I am a Landing Page</Text>
+           <Text cls='bg-red' style={styles.text}>{txt}</Text>
+        <Text cls='bg-red'>{txt2}</Text>
+          <View>
+            <TouchableOpacity>
+              <Octicons name='bookmark' size={32} />
+            </TouchableOpacity>
+          </View>
       </View>
     )
   }
@@ -26,6 +37,9 @@ const styles = StyleSheet.create({
   backgroundImage: {
     resizeMode: 'contain',
     opacity: 0.90
+  },
+  text: {
+    height: 8
   }
 })
 
